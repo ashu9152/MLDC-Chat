ed2bca0a1529ef1d3ad9ab530f55d9c7e0a7c30e
Ashut <omkarashu28@gmail.com>
2021-04-14 20:43:18 +0530

    Created user list ui, View holder for recyclerview and fetch user info from firbase

---
+++ app/src/main/java/com/example/mldcchat/fragments/UserViewHolder.kt
@@ -0,0 +1,12 @@
+package com.example.mldcchat.fragments
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.auth.User
+
+class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    fun bind(user: User){
+
+    }
+}
